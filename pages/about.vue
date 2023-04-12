9e626834408e400ffc28df4f566eca66c2b29122
Brijesh Prajapati <brij.p98@gmail.com>
2023-04-11 23:23:38 -0700

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h2>About page</h2>
+      <p>Some stuff </p>
+      <p>Some more stuff that goes here</p>
+  
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+    h2{
+      margin-bottom: 20px;
+      font-size: 36px;
+    }
+  
+    p {
+      margin: 20px 0;
+    }
+  </style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Some random text goes here.</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params;
+</script>
+
+<style scoped>
+
+</style>
